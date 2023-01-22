bef1029e56d989c791423d817430111916fd12c5
irshathahamed21 <21irshathkhan@gmail.com>
2023-01-22 12:42:09 +0530

    cors moved to just below the import statements

--- index.js
+++ index.js
@@ -15,17 +15,18 @@
  if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
   }
+  app.use(cors({
+   origin: "*",  
+   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+   allowedHeaders: ["Content-Type", "Authorization"],
+   credentials: true
+}));
+
  app.use(express.json())
  app.use(cookieParser())
  app.use(bodyParser.urlencoded({extended:true}))
  app.use(errorMiddleware)
  app.use(fileUpload())
- app.use(cors({
-    origin: "*",  
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
  
  app.get("/", async(req,res) => {
    
