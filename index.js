3708b1bc8581bbfb84537c8267c9cd0f33015a5b
irshathahamed21 <21irshathkhan@gmail.com>
2023-01-22 10:40:43 +0530

    additional options added in cors

--- index.js
+++ index.js
@@ -22,7 +22,9 @@
  app.use(fileUpload())
  app.use(cors({
     origin: "*",  
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
 }));
  
  app.get("/", async(req,res) => {
