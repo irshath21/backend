61675855638eb50bb806cbe058cbf0c12a3ec70f
irshathahamed21 <21irshathkhan@gmail.com>
2023-01-24 11:52:14 +0530

    cors function all parameters removed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
  if (process.env.NODE_ENV !== 'production') {
     dotenv.config();
   }
-app.use(cors({origin: "http://localhost:3000", credentials: true}));
+app.use(cors());
  app.use(express.json())
  app.use(cookieParser())
  app.use(bodyParser.urlencoded({extended:true}))
