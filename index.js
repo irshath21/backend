b92c71be88e88894b35b8411d070525c680b2b28
irshathahamed21 <21irshathkhan@gmail.com>
2023-01-29 18:08:30 +0530

    credentials removed from cors object

--- index.js
+++ index.js
@@ -9,10 +9,11 @@
  const errorMiddleware = require("./src/middleware/error")
  const path = require("path")
  const cors = require('cors');
+app.use(cors());
+
  if (process.env.NODE_ENV !== 'production') {
     dotenv.config();
   }
-app.use(cors());
  app.use(express.json())
  app.use(cookieParser())
  app.use(bodyParser.urlencoded({extended:true}))
