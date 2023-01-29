09b16e7f284f1fea743a7f545d41bddac3de0567
irshathahamed21 <21irshathkhan@gmail.com>
2023-01-29 19:01:27 +0530

    lines changed to remove the error

--- index.js
+++ index.js
@@ -13,9 +13,9 @@
  if (process.env.NODE_ENV !== 'production') {
     dotenv.config();
   }
- app.use(cors());
  app.use(express.json())
  app.use(cookieParser())
+ app.use(cors());
  app.use(bodyParser.urlencoded({extended:true}))
  app.use(fileUpload())
  
