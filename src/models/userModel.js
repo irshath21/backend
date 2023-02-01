10f1172d8288df6e43188b1375631849a7d6caea
irshathahamed21 <21irshathkhan@gmail.com>
2023-02-01 16:12:18 +0530

    getJWTToken method removed in user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -60,12 +60,12 @@ userSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, 10);
   });
   
-  // JWT TOKEN
-  userSchema.methods.getJWTToken = function () {
-    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRE,
-    });
-  };
+  // // JWT TOKEN
+  // userSchema.methods.getJWTToken = function () {
+  //   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+  //     expiresIn: process.env.JWT_EXPIRE,
+  //   });
+  // };
   
   // Compare Password
   
