444eac006f535e8a42a565ccde61eddd06f651a6
irshathahamed21 <21irshathkhan@gmail.com>
2023-02-11 16:03:03 +0530

    response foruser not found is added in the updatePassword function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -195,6 +195,11 @@ exports.updatePassword = async (req, res) => {
 
     try {
         const user = await User.findById(req.user.id).select("+password")
+        
+        if(!user){
+        return res.status(201).json({ message:"user not found"});
+
+        }
 
         const isPasswordMatched = await user.comparePassword(req.body.oldPassword)
 
